0924ed20eb34e074d9045fdfa96ae0ecd97be82a
Zhaoguoquan <zhaoguoquan94@gmail.com>
2014-09-09 19:29:48 +0800

    first commit to cousera mooc-cloud

--- assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/AnEmptyController.java
+++ assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/AnEmptyController.java
@@ -17,7 +17,25 @@
  */
 package org.magnum.dataup;
 
+import org.apache.commons.io.IOExceptionWithCause;
+import org.magnum.dataup.model.Video;
+import org.magnum.dataup.model.VideoStatus;
+import org.magnum.dataup.repo.VideoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import retrofit.client.Response;
+import retrofit.http.Body;
+import retrofit.http.Part;
+import retrofit.http.Path;
+import retrofit.mime.TypedFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Random;
 
 @Controller
 public class AnEmptyController {
@@ -38,5 +56,79 @@ public class AnEmptyController {
                                                                                                                                                                                                                                                                         
 	 * 
 	 */
-	
+    @Autowired
+    private VideoRepo videos;
+    @RequestMapping(value="/video",method = RequestMethod.GET)
+    public @ResponseBody Collection<Video> getVideoList() {
+
+        return videos.getVideos();
+    }
+    @RequestMapping(value = "/video",method = RequestMethod.POST)
+    public @ResponseBody Video addVideo(@RequestBody Video v) {
+        v.setId(v.hashCode()+new Date().hashCode()+1);
+        v.setDataUrl("http://localhost:8080/video/" + v.getId() + "/data");
+        videos.addVideo(v);
+        return v;
+    }
+    @RequestMapping(value = "/video/{id}/data")
+    public @ResponseBody VideoStatus setVideoData(@PathVariable long id, @RequestParam MultipartFile data) {
+        InputStream in;
+        FileOutputStream fw ;
+        try {
+             in=data.getInputStream();
+             fw = new FileOutputStream(String.valueOf(id));
+            byte[] b=new byte[1024];
+            int size=0;
+            while((size=in.read(b))!=0) {
+                fw.write(b, 0, size);
+            }
+            in.close();
+            fw.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return new VideoStatus(VideoStatus.VideoState.READY);
+
+
+
+    }
+    @RequestMapping(value = "/video/{id}/data")
+    public void getData(@PathVariable long id,HttpServletResponse response) {
+        FileInputStream in=null;
+        OutputStream os;
+        try {
+            in = new FileInputStream(String.valueOf(id));
+            os = response.getOutputStream();
+            byte[] b = new byte[1024];
+            int size=0;
+            while ((size = in.read(b)) != 0) {
+                os.write(b, 0, size);
+            }
+            in.close();
+            os.close();
+
+
+        } catch (FileNotFoundException e) {
+            response.setStatus(404);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+
+    public void setVideos(VideoRepo videos) {
+        this.videos = videos;
+    }
+
+    public VideoRepo getVideos() {
+        return videos;
+
+    }
+
 }
---
+++ assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/repo/VideoRepo.java
@@ -0,0 +1,26 @@
+package org.magnum.dataup.repo;
+
+import org.magnum.dataup.model.Video;
+
+import java.util.Collection;
+
+/**
+ * Created by 赵国铨 on 14-9-9.
+ */
+public class VideoRepo {
+    Collection<Video> videos;
+    public Collection<Video> getVideos(){
+        return videos;
+    }
+    public boolean addVideo(Video v){
+        videos.add(v);
+        return true;
+    }
+
+    // Find all videos with a matching title (e.g., Video.name)
+    public Collection<Video> findByTitle(String title){
+        return null;
+        //TODO
+    }
+
+}
